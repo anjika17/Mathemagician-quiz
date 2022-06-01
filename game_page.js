d69a7beddd33cfe7acb0fa7317fe4b86b62ca670
anjika17 <80506928+anjika17@users.noreply.github.com>
2022-06-01 11:15:02 +0530

    Add files via upload

--- game_page.js
+++ game_page.js
@@ -1,26 +1,27 @@
-player1_name = localStorage.getItem("player1_name");
-player2_name = localStorage.getItem("player2_name");
+var player_1 = localStorage.getItem("player_1_name");
+var player_2 = localStorage.getItem("player_2_name");
 
-player1_score = 0 ;
-player2_score = 0 ;
+player_1_score = 0;
+player_2_score = 0;
 
- document.getElementById("login_input_player1").innerHTML = player1_name + ":";
- document.getElementById("login_input_player2").innerHTML = player2_name + ":";
- document.getElementById("player_1score").innerHTML = player1_score ;
- document.getElementById("player_2score").innerHTML = player2_score ;
+document.getElementById("player1_name").innerHTML = player_1 + " :";
+document.getElementById("player2_name").innerHTML = player_2 + " :";
 
- value_1 = document.getElementById("value_1").value ; 
- localStorage.setItem("value_1" , value_1) ;
+document.getElementById("player_1score").innerHTML = player_1_score;
+document.getElementById("player_2score").innerHTML = player_2_score;
 
- value_2 = document.getElementById("value_2").value ; 
- localStorage.setItem("value_2" , value_2) ;
+document.getElementById("q_turn").innerHTML = "Question Turn - " + player_1;
+document.getElementById("a_turn").innerHTML = "Answer Turn - " + player_2;
 
-document.getElementById("player_question").innerHTML = "Question turn - " + player1_name ;
-document.getElementById("player_answer").innerHTML = "Answer turn - " + player2_name ;
-
-question = "<h3>"+value_1+"x"+value_2+"</h3>" ; 
-input = "<input id='answer' type='number' class='form-control'>" ;
-button = "<button id='submit' onclick='send()' type='button' class='btn btn-info'></button" ;
-
-div = question + input + button ;
-document.getElementById("output").innerHTML = div ;
+function send() {
+    value1 = document.getElementById("input_value_1").value;
+    value2 = document.getElementById("input_value_2").value;
+    actual_answer = parseInt(value1) * parseInt(value2);
+    question_word = "<h5>" +value1 + " X " + value2 + "</h5>";
+    input_box = "<br>Answer : <input type='text' id='input_check_box'>";
+    check_button = "<br><br><button class='btn btn-info' onclick='check()'>Check</button>";
+    row = question_word + input_box + check_button;
+    document.getElementById("output").innerHTML = row;
+    document.getElementById("input_value_1").value = "";
+    document.getElementById("input_value_2").value = "";
+}
